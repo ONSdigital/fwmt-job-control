0a7a1f4ea336fb0c4b9d5af7f20e68d3cfac9bcf
ThomasPootQA <thomas.poot@qa.com>
2018-09-20 16:07:28 +0100

    Amended the JSON files, corrected some malfunctions in sending jobs, and providing data files to the rabbit handler

--- lib/address_data.rb
+++ lib/address_data.rb
@@ -4,14 +4,14 @@ module AddressData
   def self.read_data_files
     @@data = []
     for filename in Dir["data/*.json"]
-      @@data << JSON.parse(File.read(filename))
+      json = JSON.parse(File.read(filename))
+      @@data << json
     end
-    p @@data
   end
 
   def self.get_data_files
     if @@data == nil
-      @@data = read_data_files
+      read_data_files
     end
 
     return @@data
