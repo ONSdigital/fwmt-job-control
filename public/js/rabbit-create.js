9e869b50decb0ba729f02acd27a01a57f12e3029
ThomasPootQA <thomas.poot@qa.com>
2018-09-20 11:23:26 +0100

    Moved addresses into their own data lists

--- public/js/rabbit-create.js
+++ public/js/rabbit-create.js
@@ -8,6 +8,18 @@ $(document).ready(function() {
         // }
     });
 
+    // radio buttons in the World form
+    function form_world_select() {
+        $("#input_world_custom").hide();
+        switch (this.id) {
+        case "radio_world_custom":
+            $("#input_world_custom").show();
+            break;
+        }
+    }
+    $("#radio_world_default, #radio_world_mendel, #radio_world_custom").change(form_world_select);
+    form_world_select.call($("#form_world input:checked").filter(":first")[0]);
+
     // radio buttons in the ID form
     function form_id_select() {
         $("#input_id_single, #input_id_list, #input_id_incr, #input_id_rand").hide();
