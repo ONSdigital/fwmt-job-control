8b14abb491416b16ed28a1506e6b15475b20b95b
ThomasPootQA <thomas.poot@qa.com>
2018-10-02 20:46:54 +0100

    Corrected the survey type from LMS to OHS

--- lib/rabbit_handler.rb
+++ lib/rabbit_handler.rb
@@ -23,18 +23,29 @@ class RabbitHandler
     @exchange.publish(json, routing_key: ROUTING_KEY, persistent: true, content_type: 'text/plain')
   end
 
+  # CCS jobs - postcode only
+
   def run(form)
     for i in 0..(form[:count].to_i - 1)
-      addresses = AddressData.get_data_files.find { |f| f["name"] == "postcodes_west" }["addresses"]
-      postcode = addresses[i % addresses.length]["postcode"]
+      addresses = AddressData.get_data_files.find { |f| f["name"] == "west" }["addresses"]
+      address = addresses[i & addresses.length]
       message = {
         actionType: "Create",
         jobIdentity: SecureRandom.hex(4),
         surveyType: form[:surveyType],
         preallocatedJob: false,
         mandatoryResourceAuthNo: nil,
-        dueDate: Time.now.utc.iso8601,
-        address: {postCode: postcode},
+        dueDate: (Time.now + (60*60*24*1)).utc.iso8601,
+        address: {
+          line1: address['line1'],
+          line2: address['line2'],
+          line3: address['line3'],
+          line4: address['line4'],
+          townName: address['townName'],
+          postCode: address['postcode'],
+          latitude: address['latitude'],
+          longitude: address['longitude'],
+        },
       }
       p "Sending message: #{message}"
       send_one(message)
