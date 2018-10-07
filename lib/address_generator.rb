c4401559f4c79eae1eba9b3d71ea7cd1172a81f5
ThomasPootQA <thomas.poot@qa.com>
2018-10-08 00:45:17 +0100

    Added file upload

--- lib/address_generator.rb
+++ lib/address_generator.rb
@@ -1,23 +1,38 @@
 class AddressGenerator
 
-  def initialize(kind, strategy, addr_single=nil, addr_preset=nil, addr_list=nil)
+  def initialize(kind, strategy, addr_single=nil, addr_preset=nil, addr_list=nil, addr_file=nil)
     raise ArgumentError "invalid kind" if not [:single, :preset, :list].include?(kind)
     @kind = kind
     raise ArgumentError "invalid strategy" if not [:random, :incremental, :once_per].include?(strategy)
     @strategy = strategy
     case @kind
     when :single
-      raise ArgumentError "no address provided" if addr_single == null
+      raise ArgumentError "no address provided" if addr_single == nil
       raise ArgumentError "address is not a hash" if not addr_single.is_a?(Hash)
       @addresses = [addr_single]
     when :preset
-      raise ArgumentError "no address preset list provided" if addr_single == null
+      raise ArgumentError "no address preset list provided" if addr_single == nil
       @addresses = AddressData.get_data_files(addr_preset)
     when :list
-      raise ArgumentError "no address list provided" if addr_single == null
+      raise ArgumentError "no address list provided" if addr_single == nil
       raise ArgumentError "address list is not an array" if not addr_preset.is_a?(Array)
       raise ArgumentError "address list contains non-hashes" if addr_preset.all? { |addr| addr.is_a?(Hash) }
       @addresses = addr_list
+    when :file
+      raise ArgumentError "no address file provided" if addr_file == nil
+      @addresses = []
+      CSV.parse(addr_file) do |row|
+        @addresses << {
+          "line1" => row[0],
+          "line2" => row[1],
+          "line3" => row[2],
+          "line4" => row[3],
+          "townName" => row[4],
+          "postcode" => row[5],
+          "latitude" => row[5],
+          "longitude" => row[6],
+        }
+      end
     end
     @index = 0
   end
@@ -36,6 +51,15 @@ class AddressGenerator
       addr = @addresses[@index % length]
     end
     @index += 1
-    return addr
+    return {
+      line1: addr['line1'],
+      line2: addr['line2'],
+      line3: addr['line3'],
+      line4: addr['line4'],
+      townName: addr['townName'],
+      postCode: addr['postcode'],
+      latitude: addr['latitude'],
+      longitude: addr['longitude'],
+    }
   end
 end
---
+++ lib/resno_generator.rb
@@ -0,0 +1,23 @@
+class ResnoGenerator
+  def initialize(kind, authno_single=nil, authno_list=nil)
+    raise ArgumentError "invalid kind" if not [:single, :list].include?(kind)
+    @kind = kind
+    case @kind
+    when :single
+      raise ArgumentError "no authno provided" if authno_single == nil
+      @authnos = [authno_single]
+    when :list
+      raise ArgumentError "no authno list provided" if authno_list == nil
+      raise ArgumentError "authno list is not an array" if not authno_list.is_a?(Array)
+      @authnos = authno_list
+    end
+  end
+
+  def length
+    return @authnos.length
+  end
+
+  def generate
+    return @authnos.sample
+  end
+end
