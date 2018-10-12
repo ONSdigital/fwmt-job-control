a36c183eed76264f48280a216b5512497a719d2b
Thomas Poot <thomas.poot@qa.com>
2018-10-12 14:31:08 +0100

    Fixed linter errors

--- lib/date_generator.rb
+++ lib/date_generator.rb
@@ -1,39 +1,47 @@
+# frozen_string_literal: true
+
 class DateGenerator
-  def initialize(kind, date=nil, hours=nil, days=nil)
-    raise ArgumentError, "invalid kind" if not ["set", "hours", "days"].include?(kind)
+  def initialize(kind, date = nil, hours = nil, days = nil)
+    raise ArgumentError, 'invalid kind' unless %w[set hours days].include?(kind)
+
     case kind
-    when "set"
+    when 'set'
       @kind = :set
-      raise ArgumentError, "no date provided" if date == nil
+      raise ArgumentError, 'no date provided' if date.nil?
+
       @date = date_parsed(date)
-    when "hours"
+    when 'hours'
       @kind = :hours
-      raise ArgumentError, "no hours provided" if hours == nil
+      raise ArgumentError, 'no hours provided' if hours.nil?
+
       @date = date_now_plus_hours(hours)
-    when "days"
+    when 'days'
       @kind = :days
-      raise ArgumentError, "no days provided" if days == nil
+      raise ArgumentError, 'no days provided' if days.nil?
+
       @date = date_now_plus_days(days)
     end
   end
 
   def date_parsed(date)
-    return Time.httpdate(date) rescue Time.parse(date)
+    Time.httpdate(date)
+  rescue ArgumentError
+    Time.parse(date)
   end
 
   def date_now
-    return Time.now.utc.iso8601
+    Time.now.utc.iso8601
   end
 
   def date_now_plus_hours(hours)
-    return (Time.now + (60 * 60 * hours.to_i)).utc.iso8601
+    (Time.now + (60 * 60 * hours.to_i)).utc.iso8601
   end
 
   def date_now_plus_days(days)
-    return (Time.now + (60 * 60 * 24 * days.to_i)).utc.iso8601
+    (Time.now + (60 * 60 * 24 * days.to_i)).utc.iso8601
   end
 
   def generate
-    return @date
+    @date
   end
 end
